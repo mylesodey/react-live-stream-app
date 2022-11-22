45759b51f16ccaab3960e8d4421ac16a2bd7b652
Mete Oyama <48647173+mylesodey@users.noreply.github.com>
2022-11-22 08:46:45 +0100

    Update google.js

--- frontend/client-server/auth-config-sample/google.js
+++ frontend/client-server/auth-config-sample/google.js
@@ -1,7 +1,7 @@
 /* Google Credentials */
 
 const googleCredentials = {
-  clientId: '',
+  clientId: '789868182723-kiur4176iu9neh435ltsn6b1e1rrbegu.apps.googleusercontent.com',
 };
 
 export default googleCredentials;
